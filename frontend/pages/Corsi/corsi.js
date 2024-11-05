ecd0a5bee824451b29eb2ffe88ff5ca1f8e5f9e9
Belba Jurgen <124259718+jurrbl@users.noreply.github.com>
2024-11-05 16:51:53 +0100

    Courses Done

--- frontend/pages/Corsi/corsi.js
+++ frontend/pages/Corsi/corsi.js
@@ -1,9 +1,8 @@
 window.onload = function() {
-    
-
     // Dropdown Animation
     const sectionTitle = document.querySelector(".section-title");
     const sectionContent = document.querySelector(".section-content");
+    
     sectionTitle.addEventListener("click", () => {
         sectionContent.classList.toggle("show");
     });
